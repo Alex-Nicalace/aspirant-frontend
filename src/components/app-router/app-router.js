791770e03e0771e3b3cd17ea98dd60acd1d03c5d
Alex <anicalace@gmail.com>
2022-05-24 13:32:56 +0300

    changed logic navigation of pages

--- src/components/app-router/app-router.js
+++ src/components/app-router/app-router.js
@@ -1,17 +1,26 @@
-import React, {useContext} from 'react';
+import React, {useEffect} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom'
 import {authRoutes, publicRoutes} from "../../routes";
-import {AspirantApiContext} from "../context/aspirant-api-context";
+import {useAspirantApiContext} from "../context/aspirant-api-context/aspirant-api-context";
+import {CircularProgress} from "@material-ui/core";
 
 // логика навигации по страницам
 const AppRouter = () => {
-        const {isAuth} = useContext(AspirantApiContext);
-        //const isAuth = true;
+        const {user: {isAuth, isLoading, auth}} = useAspirantApiContext();
+
+        useEffect(() => {
+            auth();
+        }, [])
+
+        if (isLoading)
+            return <CircularProgress />
+
         return (
             isAuth
                 ? (
-                    <Switch> {authRoutes.map(({path, render, exact}) =>
-                        <Route key={path} path={path} render={render} exact={ exact }/>)};
+                    <Switch> {authRoutes
+                        .map(({path, render, exact}) =>
+                        <Route key={path} path={path} render={render} exact={exact}/>)};
                         <Redirect to={authRoutes[0].path}/>
                     </Switch>
                 )
